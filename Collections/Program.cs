f12aad398473c01a144b37a3d87b35f9f97b490a
[prasanna] <prasanna>
2022-06-09 12:42:52 +0530

    [prasanna]; collections performing

---
+++ Collections/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Collections.CollectionsPractice practice = new Collections.CollectionsPractice();
+            practice.List();
+            practice.Stack();
+            practice.Queue();
+            practice.Set();
+            practice.Dictionary();
+        }
+    }
+}
+
